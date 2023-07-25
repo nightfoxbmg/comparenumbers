fcba56afab3171c8d8fd23716b1336ad9d69134b
nightfoxbmg <mishabalabanpw@gmail.com>
2023-07-25 18:50:16 +0300

    init commit

---
+++ numberscompare/numbersCompare.java
@@ -0,0 +1,13 @@
+package numberscompare;
+
+public class numbersCompare {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        if (a >= 10) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+}
